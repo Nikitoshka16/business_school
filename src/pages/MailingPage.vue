971a87a1f9ae2df3f54ebeda58c8be5a6b7a84e5
barbie.mgn@mail.ru <barbie.mgn@mail.ru>
2023-06-18 20:14:38 +0500

    commit

--- src/pages/MailingPage.vue
+++ src/pages/MailingPage.vue
@@ -227,7 +227,7 @@ export default {
             this.clients = this.clients.filter(client => client.sex.toLowerCase().includes(searchQuery.searchSex.toLowerCase()))
             this.clients = this.clients.filter(client => client.fullage >= searchQuery.searchAge)
             this.clients = this.clients.filter(client => client.dateofbirth.includes(searchQuery.searchDateOfBirth))
-            // this.clients = this.clients.filter(client => client.faculty.toLowerCase().includes(searchQuery.searchFaculty.toLowerCase()))
+            this.clients = this.clients.filter(client => client.faculty.toLowerCase().includes(searchQuery.searchFaculty.toLowerCase()))
             // this.clients = this.clients.filter(client => client.specialty.toLowerCase().includes(searchQuery.searchSpecialty.toLowerCase()))
             // this.clients = this.clients.filter(client => client.university.toLowerCase().includes(searchQuery.searchUniversity.toLowerCase()))
         },
